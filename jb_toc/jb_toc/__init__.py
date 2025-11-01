2eb9e0220b8aa476a0b85ea4f3d966e3e430753f
Alex-Lewandowski <aflewandowski@alaska.edu>
2025-11-01 10:06:54 -0800

    add get_titles test

--- jb_toc/jb_toc/__init__.py
+++ jb_toc/jb_toc/__init__.py
@@ -4,6 +4,6 @@ from .titles import get_titles
 def _jupyter_server_extension_points():
     return [{"module": "jb_toc"}]
 
-def load_jupyter_server_extension(server_app):
+def _load_jupyter_server_extension(server_app):
     setup_handlers(server_app.web_app)
     server_app.log.info("jb_toc loaded: /jbtoc/titles and /jbtoc/health ready")
